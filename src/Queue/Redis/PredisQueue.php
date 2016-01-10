c163445373c3c12466552c20514a03450a4c8860
Alexandru Patranescu <drealecs@gmail.com>
2016-01-11 01:19:08 +0200

    add predis implementation

---
+++ src/Queue/Redis/PredisQueue.php
@@ -0,0 +1,195 @@
+<?php
+namespace ParallelTask\Queue\Redis;
+
+use ParallelTask\Queue\InputMessage;
+use ParallelTask\Queue\InputMessageIdentifier;
+use ParallelTask\Queue\NonCallbackQueue;
+use ParallelTask\Queue\OutputMessage;
+use ParallelTask\Queue\InputMessageWithIdentifier;
+use Predis\Client;
+use Predis\ClientContextInterface;
+use Ramsey\Uuid\Uuid;
+
+class PredisQueue extends NonCallbackQueue
+{
+    /**
+     * @var int
+     */
+    private $messageTimeout = 60;
+
+    /**
+     * @var Client
+     */
+    private $redis;
+
+    public function __construct(Client $redis)
+    {
+        $this->redis = $redis;
+    }
+
+    /**
+     * @param $type
+     * @param InputMessage $inputMessage
+     */
+    public function putInput($type, InputMessage $inputMessage)
+    {
+        $id = Uuid::uuid4()->toString();
+        $data = $inputMessage->getData();
+
+        $this->storeInput($type, $id, $data, false);
+    }
+
+    /**
+     * @param $type
+     * @param InputMessage $inputMessage
+     * @return InputMessageIdentifier
+     */
+    public function submitInput($type, InputMessage $inputMessage)
+    {
+        $id = Uuid::uuid4()->toString();
+        $data = $inputMessage->getData();
+
+        $this->storeInput($type, $id, $data, true);
+
+        return new InputMessageIdentifier($id);
+    }
+
+    private function storeInput($type, $id, $data, $captureResult)
+    {
+        $this->redis->transaction(function (ClientContextInterface $redis) use ($type, $id, $data, $captureResult) {
+            $redis->hset($this->getMessageKey($type), $id, $data);
+            $redis->lpush($this->getMessageQueueKey($type), $id);
+            $redis->hset($this->getMessageQueueTimeKey($type), $id, time());
+            if ($captureResult) {
+                $redis->hset($this->getMessageResultKey($type), $id, 'MessageNotFinished');
+            }
+        });
+    }
+
+    /**
+     * @param string $type
+     * @return InputMessageWithIdentifier
+     */
+    public function getInput($type)
+    {
+        if (rand(0, 99) == 0) {
+            $this->requeueOldWorkingMessages($type);
+        }
+
+        do {
+            $messageId = $this->redis->brpoplpush($this->getMessageQueueKey($type), $this->getMessageRunKey($type), 1);
+            if (empty($messageId) && rand(0, 9) == 0) {
+                $this->requeueOldWorkingMessages($type);
+            }
+        } while (empty($messageId));
+        $this->redis->hset($this->getMessageStartTimeKey($type), $messageId, time());
+
+        $data = $this->redis->hget($this->getMessageKey($type), $messageId);
+        $inputMessage = new InputMessage($data);
+        $identifier = new InputMessageIdentifier($messageId);
+
+        return new InputMessageWithIdentifier($identifier, $inputMessage);
+    }
+
+    /**
+     * @param string $type
+     * @param InputMessageIdentifier $identifier
+     * @param OutputMessage $outputMessage
+     */
+    public function putOutput($type, InputMessageIdentifier $identifier, OutputMessage $outputMessage)
+    {
+        $messageId = $identifier->getId();
+
+        $captureResult = 'MessageNotFinished' === $this->redis->hget($this->getMessageResultKey($type), $messageId);
+
+        $this->redis->transaction(function (ClientContextInterface $redis) use ($type, $messageId, $outputMessage, $captureResult) {
+            $redis->lrem($this->getMessageRunKey($type), 0, $messageId);
+            $redis->hdel($this->getMessageStartTimeKey($type), $messageId);
+            $redis->lrem($this->getMessageQueueKey($type), 0, $messageId);
+            $redis->hdel($this->getMessageQueueTimeKey($type), $messageId);
+            $redis->hdel($this->getMessageKey($type), $messageId);
+            if ($captureResult) {
+                $data = $outputMessage->getData();
+                $redis->hset($this->getMessageResultKey($type), $messageId, $data);
+                $redis->lpush($this->getMessageResultReadyKey($type, $messageId), 'true');
+            }
+        });
+
+    }
+
+    /**
+     * @param string $type
+     * @param InputMessageIdentifier $identifier
+     * @return OutputMessage
+     */
+    public function getOutput($type, InputMessageIdentifier $identifier)
+    {
+        $messageId = $identifier->getId();
+
+        $data = $this->redis->hget($this->getMessageResultKey($type), $messageId);
+        if (empty($data)) {
+            return null;
+        }
+
+        do {
+            $resultReady = $this->redis->blpop($this->getMessageResultReadyKey($type, $messageId), 1);
+        } while (empty($resultReady));
+
+        $data = $this->redis->hget($this->getMessageResultKey($type), $messageId);
+        $this->redis->hdel($this->getMessageResultKey($type), $messageId);
+
+        return new OutputMessage($data);
+    }
+
+    private function requeueOldWorkingMessages($type)
+    {
+        $messageIds = array_unique($this->redis->lrange($this->getMessageRunKey($type), 0, -1));
+        foreach ($messageIds as $messageId) {
+            $time = $this->redis->hget($this->getMessageStartTimeKey($type), $messageId);
+            if (!empty($time) && time() > $this->messageTimeout + (int)$time) {
+                $this->redis->transaction(function (ClientContextInterface $redis) use ($type, $messageId) {
+                    $redis->rpush($this->getMessageQueueKey($type), $messageId);
+                    $redis->lrem($this->getMessageRunKey($type), 1, $messageId);
+                    $redis->hdel($this->getMessageStartTimeKey($type), $messageId);
+                });
+            }
+        }
+    }
+
+    private function getMessageRunKey($type)
+    {
+        return 'thread-worker-run-' . $type;
+    }
+
+    private function getMessageStartTimeKey($type)
+    {
+        return 'thread-worker-start-time-' . $type;
+    }
+
+    private function getMessageResultReadyKey($type, $messageId)
+    {
+        return 'thread-worker-result-ready-' . $type . '-' . $messageId;
+    }
+
+    private function getMessageKey($type)
+    {
+        return 'thread-worker-message-' . $type;
+    }
+
+    private function getMessageQueueKey($type)
+    {
+        return 'thread-worker-queue-' . $type;
+    }
+
+    private function getMessageQueueTimeKey($type)
+    {
+        return 'thread-worker-queue-time-' . $type;
+    }
+
+    private function getMessageResultKey($type)
+    {
+        return 'thread-worker-result-' . $type;
+    }
+
+
+}
---
+++ test/Queue/PredisQueueTest.php
@@ -0,0 +1,16 @@
+<?php
+namespace ParallelTask\Queue;
+
+use ParallelTask\Queue\Redis\PredisQueue;
+use Predis\Client;
+
+class PredisQueueTest extends QueueTest
+{
+
+    protected function getQueueInstance()
+    {
+        $client = new Client();
+
+        return new PredisQueue($client);
+    }
+}
