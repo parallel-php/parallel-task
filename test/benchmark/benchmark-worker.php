c9dc592553b7885c27e2a9866360c97758729391
Alexandru Patranescu <drealecs@gmail.com>
2016-10-08 23:18:22 +0300

    fix worker benchmark and add it to travis

--- test/benchmark/benchmark-worker.php
+++ test/benchmark/benchmark-worker.php
@@ -16,6 +16,7 @@ $queueFactory = new QueueFactory();
 $queue = $queueFactory->make($queueType);
 $transformer = new TaskMessageSerializeTransformer();
 $taskFactory = new TaskFactorySimple();
-$worker = new Worker(new TaskRunner($queue, $transformer, $taskFactory, $transformer));
+$supervisor = new \ParallelTask\Task\TaskRunnerNullSupervisor();
+$worker = new Worker(new TaskRunner($queue, $transformer, $taskFactory, $transformer, $supervisor));
 
 $worker->work($type);
